0d8d7f0109d4b1f1a3d95a8e1deec988b7e0fbe8
Hritik Jaiswal <hritik.jaiswal@somaiya.edu>
2020-10-29 13:22:02 +0530

    updated

--- src/11 - Otsu thresolding/Otsu Thresholding.py
+++ src/11 - Otsu thresolding/Otsu Thresholding.py
@@ -2,22 +2,24 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
 
-img = cv2.imread('../Images and Videos/otsu.jpg',0)
+img = cv2.imread('../Images and Videos/otsu.jpg')
 
-ret1, thresh1 = cv2.threshold(img,127,255, cv2.THRESH_BINARY)
+grayscale = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-ret2, thresh2 = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+ret1, thresh1 = cv2.threshold(grayscale,127,255, cv2.THRESH_BINARY)
 
-gauss_img = cv2.GaussianBlur(img, (5,5), 0)
+ret2, thresh2 = cv2.threshold(grayscale, 127, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+gauss_img = cv2.GaussianBlur(grayscale, (5,5), 0)
 ret3, thresh3 = cv2.threshold(gauss_img, 127,255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-images = [thresh1, thresh2, thresh3]
+images = [img,thresh1, thresh2, thresh3]
 
-titles = ['Global Thresholding (v=127)', "Otsu's Thresholding", "Otsu's Thresholding with Gaussian Blur"]
+titles = ['Original image','Global Thresholding (v=127)', "Otsu's Thresholding", "Otsu's Thresholding with Gaussian Blur"]
 
 plt.figure(figsize=(10,5))
-for i in range(3):
-    plt.subplot(2,3,(i+1))
+for i in range(4):
+    plt.subplot(2,2,(i+1))
     plt.imshow(images[i],'gray')
     plt.xlabel(titles[i])
 
